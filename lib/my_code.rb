8b03dbbcddecb9eef3b36eb825bbd53ad3ac6472
andrew kim <andrewk1216@gmail.com>
2019-08-21 00:53:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map (ary)
+  new = []
+  ary.each_with_index do |val, index| 
+    new[index] = yield val
+  end
+  return new
+end
+
+def reduce(ary, start = nil)
+  if start 
+    return_value = start
+    
+    ary.each do |value|
+      return_value = yield(return_value, value)
+    end
+  else
+    return_value = ary[0]
+    
+    ary[1..-1].each do |value|
+      return_value = yield(return_value, value)
+    end
+  end
+
+  return return_value
+end
